390d3eb7bfceb454ad7654a8bd324f4fa3cc413f
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-08-07 22:32:41 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let bgColor = null;
+
+const change = () => {
+    return body.style.backgroundColor = `${getRandomColor()}`;
+};
+
+const stop = () => {
+    clearInterval(bgColor);
+    startBtn.disabled = false;
+};
+
+startBtn.addEventListener('click', () => {
+    bgColor = setInterval(change, 1000);
+    startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', stop);
